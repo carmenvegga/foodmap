fc212fa460651daf17b90d71829266681d95b060
carmenvegga <maricarmenvegaislas@gmail.com>
2018-01-23 08:54:34 -0600

    Subiendo primeros cambios

---
+++ js/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    setTimeout(function() {
+        $(".splash-foodmap").fadeOut(1000);
+    },2000);
+
+    setTimeout(function() {
+        $(".main-foodmap").fadeIn(1000);
+    },2000);
+});
---
+++ js/data.js
@@ -0,0 +1,19 @@
+var data =
+  [
+    {
+      option: 'sopa',
+      photo: 'assets/images/soup.jpg',
+        },
+    {
+      option: 'ensalada',
+      photo: 'assets/images/salad.jpeg',
+    },
+    {
+      option 'pizza',
+      photo: 'assets/images/pizza.jpeg',
+    },
+    {
+      option 'sushi',
+      photo: 'assets/images/sushi.jpg',
+    },
+  ]
